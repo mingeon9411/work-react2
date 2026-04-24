f69a46dad420e9187a3b24030c56f086f593d392
seungjin <xeungjin@nate.com>
2026-04-24 16:27:38 +0900

    ~210page

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -1,23 +1,10 @@
+import "./Body.css";
+
 function Body() {
-    const numA = 1;
-    const numB = 2;
-    const strA = "안녕";
-    const strB = "리액트";
-    const boolA = true;
-    const boolB = false;
-    const objA = {
-        a: 1,
-        b: 2,
-    }
     return (
-        <div>
-            <h1>body</h1>
-            <h2>{numA + numB}</h2>
-            <h2>{strA + strB}</h2>
-            <h2>{String(boolA || boolB)}</h2>
-            <h2>{objA.a}</h2>
-            <h2>{objA.b}</h2>
-        </div>
-    )
+       <div className="body">
+        <h1>body</h1>
+       </div>
+    );
 }
 export default Body;
