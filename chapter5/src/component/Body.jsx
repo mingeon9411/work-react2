d1ae8704d7b932b2d81082e50ca9732588bc7479
seungjin <xeungjin@nate.com>
2026-04-27 15:38:55 +0900

    ~ 242page

--- chapter5/src/component/Body.jsx
+++ chapter5/src/component/Body.jsx
@@ -2,14 +2,57 @@ import { useState } from "react";
 import "./Body.css";
 
 function Body() {
-    const [text, setText] = useState("");
+    // const [name, setName] = useState("");
+    // const [gender, setGender] = useState("");
+    // const [birth, setBirth] = useState("");
+    // const [bio, setBio] = useState("");
+
+    const [state, setState] = useState({
+        name: "",
+        gender: "",
+        birth: "",
+        bio: "",
+    });
+
     const handleOnChange = (e) => {
-        console.log("변경된 값: ", e.target.value);
-        setText(e.target.value);
+        console.log("현재 수정 대상: ", e.target.name);
+        console.log("수정값: ", e.target.value);
+        setState({
+            ...state,
+            [e.target.name]: e.target.value,
+        });
     };
+
+    // const onChangeName = (e) => {
+    //     setName(e.target.value);
+    // };
+    // const onChangeGender = (e) => {
+    //     setGender(e.target.value);
+    // };
+    // const onChangeBirth = (e) => {
+    //     setBirth(e.target.value);
+    // };
+    // const onChangeBio = (e) => {
+    //     setBio(e.target.value);
+    // };
     return (
        <div className="body">
-        <textarea value={text} onChange={handleOnChange}/>
+        <div>
+            <input name="name" value={state.name} onChange={handleOnChange} placeholder="이름"/>
+        </div>
+        <div>
+            <select name="gender" value={state.gender} onChange={handleOnChange}>
+                <option key={""}></option>
+                <option key={"남성"}>남성</option>
+                <option key={"여성"}>여성</option>
+            </select>
+        </div>
+        <div>
+            <input name="birth" type="date" value={state.birth} onChange={handleOnChange}/>
+        </div>
+        <div>
+            <textarea name="bio" value={state.bio} onChange={handleOnChange}/>
+        </div>
        </div>
     );
 }
