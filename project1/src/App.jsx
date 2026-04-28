8849288e4e76da9f073930ace8da7f75690e7d81
seungjin <xeungjin@nate.com>
2026-04-28 12:45:58 +0900

    ~ 258page

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Viewer from './component/Viewer'
 
 function App() {
   return (
     <div className='App'>
-
+      <h1>Simple Counter</h1>
+      <section>
+        <Viewer />
+      </section>
     </div>
   )
 }
