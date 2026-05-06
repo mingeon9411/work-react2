787dd751e7cb44b59f96acb71b90b3b8f9f09208
mingeon9411 <toube094@naver.com>
2026-05-06 17:50:04 +0900

    20260506 수업5

--- project3/src/component/Editor.jsx
+++ project3/src/component/Editor.jsx
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import "./Editor.css";
+import Button from "./Button";
+import { getFormattedDate } from "../util";
 
 const Editor = ({ initData, onSubmit }) => {
+  const [state, setState] = useState({
+    date: getFormattedDate(new Date()),
+    emotionId: 3,
+    content: "",
+  });
+
+  const handleChangeDate = (e) => {
+    setState({
+      ...state,
+      date: e.target.value,
+    });
+  };
+
+  const handleChangeContent = (e) => {
+    setState({
+      ...state,
+      content: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    onSubmit(state);
+  };
+
   return (
     <div className="Editor">
       <div className="editor_section">
         {/* 날짜 */}
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          type="date"
+          value={state.date}
+          onChange={handleChangeDate}
+        />
       </div>
 
       <div className="editor_section">
@@ -20,13 +51,21 @@ const Editor = ({ initData, onSubmit }) => {
       <div className="editor_section">
         {/* 일기 */}
         <h4>오늘의 일기</h4>
-        <textarea placeholder="오늘은 어땠나요?" />
+        <textarea
+          value={state.content}
+          onChange={handleChangeContent}
+          placeholder="오늘은 어땠나요?"
+        />
       </div>
 
       <div className="editor_section bottom_section">
         {/* 작성 완료, 취소 */}
-        <button>취소하기</button>
-        <button>작성완료</button>
+        <Button text={"취소하기"} />
+        <Button
+          text={"작성 완료"}
+          type={"positive"}
+          onClick={handleSubmit}
+        />
       </div>
     </div>
   );
--- project3/src/util.js
+++ project3/src/util.js
@@ -22,3 +22,16 @@ export const getEmotionImgById = (emotionId) => {
             return null;
     }
 };
+
+export const getFomattedDate = (targetDate) => {
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth() +1;
+    let date = targetDate.getDate();
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if (date < 10) {
+        date = `0${date}`;
+    }
+    return `${year}-${month}-${date}`;
+};
