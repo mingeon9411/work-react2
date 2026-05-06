6c303b223e4c5f4cc7453839a1a3c80b3e9668a9
mingeon9411 <toube094@naver.com>
2026-05-06 14:10:44 +0900

    20260506 수업 4

--- project3/src/component/Button.jsx
+++ project3/src/component/Button.jsx
@@ -1,6 +1,9 @@
-import "./Button.css";
-
-const Button = ({ text, type, onClick }) => {
-    return <button className="Button">버튼</button>;
+const Button = ({ text, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  );
 };
+
 export default Button;
