8a817af1b8f6fb01a39e6daa4c194befa5e7b8b4
mingeon9411 <toube094@naver.com>
2026-05-06 10:55:53 +0900

    20260506수업

--- project3/src/App.jsx
+++ project3/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import emotion1 from "./assets/emotion1.png";
 
 function App() {
   return (
     <div className="App">
-      <h1>감정 일기장</h1>
+      <img alt="감정1" src={emotion1} />
     </div>
   );
 }
